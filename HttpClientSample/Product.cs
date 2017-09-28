13b1bc2be5922e1836ef410828f6d976cd7b75a5
fsprogamer <fsprogamer@gmail.com>
2017-09-28 22:42:34 +0300

    xml magic

---
+++ HttpClientSample/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.UI;
+using System.Runtime.Serialization;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace HttpClientSample
+{
+    [Serializable, XmlRoot(ElementName = "QQQ",DataType ="string", IsNullable = true, Namespace ="www.yandex.ru")]
+    public class Product
+    {
+        public string Id { get; set; }
+        [XmlElement("SuperName")]
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+}
---
+++ HttpClientSample/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace HttpClientSample
+{
+    public class Utf8StringWriter : StringWriter
+    {
+        public override Encoding Encoding => Encoding.UTF8;
+    }
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+
+        static void ShowProduct(Product product)
+        {
+            Console.WriteLine($"Name: {product.Name}\tPrice: {product.Price}\tCategory: {product.Category}");
+        }
+
+        static async Task<Uri> CreateProductAsync(Product product)
+        {
+            #region
+            //string xml;
+            //XmlSerializer s = new XmlSerializer(typeof(Product));
+            //XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+            //ns.Add("xsd", "http://www.w3.org/2001/XMLSchema");
+            //ns.Add("xsi", "http://www.w3.org/2001/XMLSchema-instance");
+
+            //using (StringWriter sw = new Utf8StringWriter())
+            //{
+            //    s.Serialize(sw, product, ns);
+            //    xml = sw.ToString();
+            //}            
+
+            //var stringContent = new StringContent(xml, Encoding.UTF8, "application/xml");
+
+            //HttpResponseMessage response = await client.PostAsync("api/products", stringContent);
+            #endregion
+
+            HttpResponseMessage response = await client.PostAsync("api/products", product, new XmlMediaTypeFormatter { UseXmlSerializer = true });
+            response.EnsureSuccessStatusCode();
+
+            // return URI of the created resource.
+            return response.Headers.Location;
+        }
+
+        static async Task<Product> GetProductAsync(string path)
+        {
+            Product product = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                product = await response.Content.ReadAsAsync<Product>();
+            }
+            return product;
+        }
+
+        static async Task<Product> UpdateProductAsync(Product product)
+        {
+            HttpResponseMessage response = await client.PutAsXmlAsync($"api/products/{product.Id}", product);
+            response.EnsureSuccessStatusCode();
+
+            // Deserialize the updated product from the response body.
+            product = await response.Content.ReadAsAsync<Product>();
+            return product;
+        }
+
+        static async Task<HttpStatusCode> DeleteProductAsync(string id)
+        {
+            HttpResponseMessage response = await client.DeleteAsync($"api/products/{id}");
+            return response.StatusCode;
+        }
+
+        static void Main()
+        {
+            RunAsync().Wait();
+        }
+
+        static async Task RunAsync()
+        {
+            client.BaseAddress = new Uri("http://localhost:55268/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
+
+            try
+            {
+                // Create a new product
+                Product product = new Product { Name = "Gizmo", Price = 100, Category = "Widgets" };
+
+                var url = await CreateProductAsync(product);
+                Console.WriteLine($"Created at {url}");
+
+                // Get the product
+                product = await GetProductAsync(url.PathAndQuery);
+                ShowProduct(product);
+
+                // Update the product
+                Console.WriteLine("Updating price...");
+                //product.Price = 80;
+                await UpdateProductAsync(product);
+
+                // Get the updated product
+                product = await GetProductAsync(url.PathAndQuery);
+                ShowProduct(product);
+
+                // Delete the product
+                var statusCode = await DeleteProductAsync(product.Id);
+                Console.WriteLine($"Deleted (HTTP Status = {(int)statusCode})");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
